563f06faf57d77cda9bf1a393337722150800a85
leelew <sysulewlee1@gmail.com>
2021-10-14 21:09:39 +0800

    @feat new train strategy

--- src/model/lstm.py
+++ src/model/lstm.py
@@ -2,28 +2,29 @@
 
 import tensorflow as tf
 
+"""
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+for gpu in gpus:
+    tf.config.experimental.set_visible_devices(
+        devices=gpus[1], device_type='GPU')
+    tf.config.experimental.set_memory_growth(device=gpu, enable=True)
 
-def lstm(n_feature, input_len):
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
+"""
 
-    gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
-    for gpu in gpus:
-        tf.config.experimental.set_visible_devices(
-            devices=gpus[1], device_type='GPU')
-        tf.config.experimental.set_memory_growth(device=gpu, enable=True)
 
-    strategy = tf.distribute.MirroredStrategy()
-    with strategy.scope():
+def lstm():
 
-        # inputs
-        inputs = tf.keras.layers.Input(shape=(input_len, n_feature))
-        x = tf.keras.layers.LSTM(units=256, return_sequences=True)(inputs)
-        x = tf.keras.layers.LSTM(units=256, return_sequences=False)(x)
-        x = tf.keras.layers.Dense(1)(x)
+    # inputs
+    inputs = tf.keras.layers.Input(shape=(1, 42))
+    x = tf.keras.layers.LSTM(units=256, return_sequences=True)(inputs)
+    x = tf.keras.layers.Dense(6)(x)
 
-        # build
-        model = tf.keras.models.Model(inputs=inputs, outputs=x)
+    # build
+    model = tf.keras.models.Model(inputs=inputs, outputs=x)
 
-        # summary
-        model.summary()
+    # summary
+    model.summary()
 
     return model
